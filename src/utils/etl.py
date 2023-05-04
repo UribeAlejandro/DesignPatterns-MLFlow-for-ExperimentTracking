59c8552c399d095d7ab4d973f06603bb92a48a37
Alejandro Uribe <jauriberamirez@gmail.com>
2023-05-03 21:22:00 -0300

    In progress: Model

--- src/utils/etl.py
+++ src/utils/etl.py
@@ -1,8 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# --------------------------------------------------------------------------- #
+# Created By: Uribe
+# Created Date: 28/4/23
+# --------------------------------------------------------------------------- #
+# ** Description **
+
+"""
+Extract, Transform and Load for datasets from openml.
+"""
+
+# --------------------------------------------------------------------------- #
+# ** Required libraries **
+
 from pandas import concat, DataFrame, Series
 from pathlib import Path
 from numpy import uint8
 from sklearn.datasets import fetch_openml
 from typing import Union, Tuple
+
+__all__ = ['extract_data', 'transform_data', 'load_data']
 
 
 def extract_data(dataset_id: Union[str, Path], data_home: Union[str, Path]) -> Tuple[DataFrame, Series]:
